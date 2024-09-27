b83b2ee86e81f628122f7013452408370b642c2d
Ganesh <Ganesh@LAPTOP-OJSV7FHM>
2024-09-27 21:45:04 +0530

    Second commit

--- src/com/bank/service/NotificationService.java
+++ src/com/bank/service/NotificationService.java
@@ -4,7 +4,6 @@ public class NotificationService {
 
 	// Method to send notifications
 	public void sendTransactionNotification(String customerId, String message) {
-		// In a real application, this could send an email or SMS.
 		System.out.println("Notification for Customer ID " + customerId + ": " + message);
 	}
 }
